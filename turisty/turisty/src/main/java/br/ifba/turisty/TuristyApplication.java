a78cf65358b3b89fc9c33ad8c06ea63db3dc66b1
wsprego <wesleysilva.santos076@gmail.com>
2024-08-26 21:13:45 -0300

    inicio do projeto
    lembrar do application

---
+++ turisty/turisty/src/main/java/br/ifba/turisty/TuristyApplication.java
@@ -0,0 +1,13 @@
+package br.ifba.turisty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristyApplication.class, args);
+	}
+
+}
---
+++ turisty/turisty/src/test/java/br/ifba/turisty/TuristyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifba.turisty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
