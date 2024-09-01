e905f0f5d39fa258192f10000d33d10e5c95c069
wsprego <wesleysilva.santos076@gmail.com>
2024-09-01 20:50:49 -0300

    crud de lugares

---
+++ turisty/turisty/src/main/java/br/ifba/turisty/domain/service/LugarService.java
@@ -0,0 +1,32 @@
+package br.ifba.turisty.domain.service;
+
+import br.ifba.turisty.domain.model.Lugar;
+import br.ifba.turisty.domain.repository.LugarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LugarService {
+
+    @Autowired
+    private LugarRepository lugarRepository;
+
+    public List<Lugar> findAll() {
+        return lugarRepository.findAll();
+    }
+
+    public Optional<Lugar> findById(Long id) {
+        return lugarRepository.findById(id);
+    }
+
+    public Lugar save(Lugar lugar) {
+        return lugarRepository.save(lugar);
+    }
+
+    public void deleteById(Long id) {
+        lugarRepository.deleteById(id);
+    }
+}
